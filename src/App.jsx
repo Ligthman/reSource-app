842a85b7cdedab2ce7d33dce820c7a3995443bc5
Ligthman <65736608+Ligthman@users.noreply.github.com>
2026-05-07 16:07:43 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -797,7 +797,7 @@ export default function ResourceApp() {
         src="/logo.png"
         alt="reSource app"
         style={{
-          width:"220px",
+          width:"240px",
           maxWidth:"85%",
           height:"auto",
           objectFit:"contain"
